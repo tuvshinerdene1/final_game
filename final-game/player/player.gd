74f30543dff1e3bb36d4873508d543921885204c
tuvshinerdene1 <tuvshin.erdene25@gmail.com>
2025-11-24 12:14:31 +0800

    added sample scene

--- final-game/player/player.gd
+++ final-game/player/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 @export var mouse_sensitivity : float = 0.002
 @export var SPEED = 5.0
 @export var JUMP_VELOCITY = 4.5
+@export var HEADBOB = true
+@export var HEADBOB_FREQUENCY = 0.5
+@export var HEADBOB_DISTANCE = 0.2
 
 var gravity:float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var camera: Camera3D = $Camera3D
@@ -21,12 +24,6 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
