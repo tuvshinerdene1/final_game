6b321c2ba71b9203c81f4266e2c8437fe3a510db
tuvshinerdene1 <tuvshin.erdene25@gmail.com>
2025-12-11 17:24:39 +0800

    radio done shit

--- final-game/radio/radio.gd
+++ final-game/radio/radio.gd
@@ -1,15 +1,64 @@
-@tool
-
-
 extends Node3D
+
+# The "Creepy/Story" audio (plays once when you get close)
 @export var audio_name : AudioStream = null
+# The "Static/Background" audio (loops after the first one finishes)
+@export var default_name: AudioStream = null
 
+# Reference to the player to calculate distance
+@export var player: Node3D = null
 
-# Called when the node enters the scene tree for the first time.
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
+
+# State variables
+var has_triggered = false
+var is_looping_stage = false
+
 func _ready() -> void:
-	pass # Replace with function body.
+	# Don't play anything at the start. We wait for the player.
+	audio_player.stop()
+	
+	# Connect the signal for when audio finishes
+	if not audio_player.finished.is_connected(_on_audio_finished):
+		audio_player.finished.connect(_on_audio_finished)
 
+func _process(_delta: float) -> void:
+	# If we have already triggered the radio, stop checking distance to save performance
+	if has_triggered:
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Ensure player and audio_player exist before checking
+	if player and audio_player:
+		# Calculate distance between Radio and Player
+		var dist = global_position.distance_to(player.global_position)
+		
+		# Check if inside the max_distance
+		if dist <= audio_player.max_distance:
+			_start_broadcast()
+
+func _start_broadcast() -> void:
+	has_triggered = true
+	
+	if audio_name:
+		# Play the Intro (Creepy Audio)
+		audio_player.stream = audio_name
+		audio_player.play()
+	elif default_name:
+		# If no intro exists, skip straight to loop
+		_play_loop()
+
+func _on_audio_finished() -> void:
+	# If we haven't reached the loop stage yet, that means the Intro just finished.
+	if not is_looping_stage:
+		_play_loop()
+	else:
+		# We are already in the loop stage. 
+		# If the audio file itself isn't set to "Loop" in Import settings,
+		# this forces it to play again immediately.
+		audio_player.play()
+
+func _play_loop() -> void:
+	if default_name:
+		is_looping_stage = true
+		audio_player.stream = default_name
+		audio_player.play()
